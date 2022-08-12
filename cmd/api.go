36ffc8b1d40c3d0aa3c2e2fc67a1e80fa5c3315e
Petr Zenin <mr@adminoid.com>
2022-08-12 18:57:26 +0300

    added gitignore and created project_path for checking project root path

---
+++ cmd/api.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/adminoid/vuego/pkg/project_path"
+)
+
+func main() {
+	fmt.Println("----------------")
+	fmt.Println(project_path.Root)
+	fmt.Println("----------------")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adminoid/vuego
+
+go 1.19
